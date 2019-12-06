2abdab8d1cae31398218a088e6ce2d3cf0429048
AnaninAN <ananinpgu@mail.ru>
2019-12-06 18:20:36 +0300

    начал задание 4

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,13 @@ import 'assets/main.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Layout } from 'components/Layout'
 
-ReactDOM.render(<Layout />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Layout />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
